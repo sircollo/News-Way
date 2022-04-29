6c137f6511cca9cd128b7d5f13b643802aeb30c6
Collins Sirwani <sircollo71@gmail.com>
2022-04-30 01:39:34 +0300

    This will define base url for the news sources

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ from instance.config import NEWS_API_KEY
 
 class Config:
   
-  NEWS_API_KEY = os.environ.get('NEWS_API_KEY') 
+  NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+  NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
 
 
 
