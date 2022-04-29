4f72f7886a80350e76610bd2520c40b198f2a872
Collins Sirwani <sircollo71@gmail.com>
2022-04-30 01:37:15 +0300

    This will use manager function to call a test function to run unittests

--- manage.py
+++ manage.py
@@ -7,6 +7,15 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
+@manager.command
+def test():
+  '''
+  run unittests
+  '''
+  import unittest
+  tests = unittest.TestLoader().discover('tests')
+  unittest.TextTestRunner(verbosity=2).run(tests)
+
 if __name__=="__main__":
   manager.run()
 
