394f9864f722fe178504b611e9f576365c26b0f2
Collins Sirwani <sircollo71@gmail.com>
2022-04-30 22:56:07 +0300

    This will define more variables to get news from different categories

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from . import main
 from ..requests import get_news
-from app.models import sources
+from ..models import sources
 #views
 @main.route('/')
 def index():
@@ -9,5 +9,11 @@ def index():
   View function to return the index page
   '''
   title = 'News-Way'
-  all_sources = get_news('sources')
-  return render_template('index.html',title=title, sources= all_sources)
+  tech_sources = get_news('technology')
+  business_sources = get_news('business')
+  sports_sources = get_news('sports')
+  health_sources = get_news('health')
+  entertainment_sources = get_news('entertainment')
+  general_sources = get_news('general')
+  science_sources = get_news('science')
+  return render_template('index.html',title=title, tech_news = tech_sources, business_news = business_sources, sports_news = sports_sources,health_news = health_sources, entertainment_news = entertainment_sources, general_news = general_sources, science_news = science_sources)
