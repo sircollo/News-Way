0c8a5e53ba88b9ead70f29705f67857fc8f22417
Collins Sirwani <sircollo71@gmail.com>
2022-04-30 03:50:38 +0300

    This will define the base url for the application

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ from instance.config import NEWS_API_KEY
 
 class Config:
   
+  NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/{}?apiKey={}'
   NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-  NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+  
 
 
 
