3e66fc497604527d5b372340cff3431e8b224daf
Collins Sirwani <sircollo71@gmail.com>
2022-05-02 17:21:19 +0300

    This will initialize bootstrap extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from config import config_options
+from flask_bootstrap import Bootstrap
 
-
+bootstrap = Bootstrap()
 def create_app(config_name):
   
   app = Flask(__name__)
   
   #create app configurations
   app.config.from_object(config_options[config_name])
+  
+  #initialize flask extensions
+  bootstrap.init_app(app)
   
   #register blueprint
   from .main import main as main_blueprint
