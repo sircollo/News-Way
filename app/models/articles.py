b66191c3424a575f150357850932f30b7d931079
Collins Sirwani <sircollo71@gmail.com>
2022-05-02 15:30:57 +0300

    Update: this will update articles objects

--- app/models/articles.py
+++ app/models/articles.py
@@ -1,14 +1,12 @@
 class Articles:
-  '''
-  Articles class to define articles objects
-  '''
-  def __init__(self,id,name,author,title,description,url,urlToImage,publishedAt,content):
-    self.id = id
-    self.name = name
-    self.author = author
-    self.title = title
-    self.description = description
-    self.url = url
-    self.urlToImage = urlToImage
-    self.publishedAt = publishedAt
-    self.content = content
+    '''
+    Articles class to define articles objects
+    '''
+    def __init__(self,id,author,title,description,url,urlToImage,publishedAt):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
